a2f38280977225ce9fbf6f760d44f5c2e9f3e4f9
Domantas Martišiūnas <domantasmart04@gmail.com>
2025-11-09 21:28:08 +0200

    Main Logic
    added main logic

---
+++ Assets/Code/Script/Health.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private int hitPoint = 2;
+    [SerializeField] private int currencyWorth = 50;
+
+    private bool isDestroyed = false;
+
+    public void TakeDamage(int damage)
+    {
+        hitPoint -= damage;
+        if(hitPoint <= 0 && !isDestroyed)
+        {
+            EnemySpawner.onEnemyDestroy.Invoke();
+            LevelManager.main.IncreaseCurrency(currencyWorth);
+            isDestroyed = true;
+            Destroy(gameObject);
+        }
+    }
+}
